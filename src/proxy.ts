c75584ec194aae346161173a9daa3e615a6f2b61
sofirdaw <sofirdaw@gmail.com>
2026-07-09 20:53:01 +0000

    mise en place du nom de domaine dans next_public_app url

--- src/proxy.ts
+++ src/proxy.ts
@@ -19,6 +19,6 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|logo.ico).*)',
   ],
 }
